8e5ca6769615c21ee67f7e5b5f1003c57b367920
xizhutao <1479069176@qq.com>
2022-08-07 16:10:42 +0800

    初始化公司列表

--- src/module-hmmm/components/companys.vue
+++ src/module-hmmm/components/companys.vue
@@ -1,12 +1,57 @@
 <template>
-  <div>企业列表</div>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <!-- 上部表单 -->
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-row :gutter="10">
+          <el-col :span="6">
+            <el-form-item label="标签名称">
+              <el-input
+                v-model="formInline.user"
+                placeholder="请输入"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="城市">
+              <el-select v-model="formInline.region" placeholder="请选择">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="地区">
+              <el-select v-model="formInline.region" placeholder="活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select> </el-form-item
+          ></el-col>
+          <el-col :span="6">
+            <el-form-item label="企业简称">
+              <el-input
+                v-model="formInline.user"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <!-- 卡片内容 -->
+    <div class="text item"></div>
+  </el-card>
 </template>
 
 <script>
 export default {
   created () { },
   data () {
-    return {}
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
   },
   methods: {},
   computed: {},
@@ -17,4 +62,26 @@ export default {
 </script>
 
 <style scoped lang='less'>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   devServer: {
-    open: true
+    // open: true
   },
   lintOnSave: false,
   publicPath: './',
